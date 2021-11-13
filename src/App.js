0ce8a73a2c92b7a2cf69c69e3544a9a943742768
Dylan <dylanhudson1998@gmail.com>
2021-11-13 16:48:25 +0000

    Fixed issue with navigation and added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
     return (
-        <div>
-            <h1>Covid tracker</h1>
-            <p>Test</p>
+        <div className='font-source-sans'>
+            <Navbar/>
+
         </div>
     )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div className='bg-charcoal p-4 text-center font-source-sans text-gray-50'>
+            <Link to='/'>
+                <h3 className='text-3xl font-normal'>
+                    <span className='font-semibold'>
+                        Covid
+                    </span>ata{" "}
+                    <i className='fas fa-viruses'/>
+                </h3>
+            </Link>
+
+            <p className='text-sm font-light mt-1'>
+                Testing Text
+            </p>
+        </div>
+    )
+}
+
+export default Navbar
